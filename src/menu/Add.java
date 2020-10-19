ddc5f2329204785d0300a0e4262e4c33ce1ecd39
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-10-19 09:55:42 -0500

    feat: deleted Action interface, added a conditional to the Add action method for overwriting a contact.

--- src/menu/Add.java
+++ src/menu/Add.java
@@ -1,8 +1,10 @@
 package menu;
 
+import contacts.Contact;
 import contacts.ContactsManager;
 import input.Input;
 
+import java.io.Console;
 import java.util.Arrays;
 
 public class Add extends MenuItem {
@@ -16,9 +18,22 @@ public class Add extends MenuItem {
         String name = Input.getString("Enter the contact's name");
         String phone = Input.getString("Enter the phone number");
 
-        ContactsManager.addData(Arrays.asList(name, phone));
-        ContactsManager.saveData();
-        System.out.println("\nAdded " + name + " to the contact list\n");
+        if(!ContactsManager.hasContact(name)) {
+            ContactsManager.addData(Arrays.asList(name, phone));
+            ContactsManager.saveData();
+            System.out.println("\nAdded " + name + " to the contact list\n");
+        } else {
+            String confirm = Input.getString(
+                    String.format("There is already a contact with the name %s, do you want to overwrite it?\n[Y/N]", name)
+            );
+            System.out.println();
+            if(confirm.equalsIgnoreCase("y") || confirm.equalsIgnoreCase("yes")) {
+                ContactsManager.addContact(new Contact(name, phone));
+            } else {
+                System.out.println("\"Add contact\" cancelled ...\n");
+            }
+
+        }
     }
 
     @Override
